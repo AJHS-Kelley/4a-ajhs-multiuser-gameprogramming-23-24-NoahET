cff8e6d904fd4cd65ac041018d1616545d0d7df4
NoahET <77453987+NoahET@users.noreply.github.com>
2023-10-03 14:31:11 -0400

    v0.0
    added program info, removed console.writeline

---
+++ multiUserGameProgramming/computer_science_exercises/02_collections/02_collections.cs
@@ -0,0 +1,13 @@
+// 02_ Collections, Thomspon Noah, 10-8-23 v0.0
+using System;
+
+namespace _02_Collections
+{
+    class Program
+    {
+        static void Main(string[] argas)
+        {
+            
+        }
+    }
+}
