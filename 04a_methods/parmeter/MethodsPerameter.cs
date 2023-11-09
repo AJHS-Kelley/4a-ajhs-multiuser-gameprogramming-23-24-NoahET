a160ba2a2fd5b26abaed3a05d279e892940caad3
NoahET <s12097572@students.duvalschools.org>
2023-11-09 14:10:48 -0500

    v.1 late project

---
+++ 04a_methods/parmeter/MethodsPerameter.cs
@@ -0,0 +1,21 @@
+Thompson Noah , program Template, v0.1
+
+NameSpace MethosdsParameters
+
+    class MethosdsParameters
+    
+        // METHOD -- named blck of code, can be used over and over.
+        // ALL methods have a SIGNATURE that defines their name, parameters, and output.
+        // Example signature
+        static void MyMethod();
+        {
+            Console.WriteLine("I like mine with lettuce and tomatoes, Heinz 57, and frence fried potatoes.\n");
+        }
+        // static - This method belongs to the current class, it is NOT an object
+        // void -This method has no return valu
+
+        
+
+
+
+  
