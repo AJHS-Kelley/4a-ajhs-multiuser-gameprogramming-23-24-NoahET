797da0110e4e13063eb0ad719e842ef86a09452e
NoahET <s12097572@students.duvalschools.org>
2023-11-09 14:19:01 -0500

    parameter v.2

---
+++ multiUserGameProgramming/computer_science_exercises/4a_methodsParameters/Methods&Parameters.cs
@@ -0,0 +1,32 @@
+Thompson Noah , program Template, v0.2
+
+NameSpace MethosdsParameters
+
+    class MethosdsParameters
+    
+        // METHOD -- named blck of code, can be used over and over.
+        // ALL methods have a SIGNATURE that defines their name, parameters, and output.
+        // Example signature
+        static void MyMethod();
+        {
+            Console.WriteLine("I like mine with lettuce and tomatoes, Heinz 57, and frence fried potatoes.\n");
+        }
+        // static - This method belongs to the current class, it is NOT an object
+        // void -- This method has no return value.
+
+        static int DoubleUp();
+        {
+            int sum = 0;
+            Console.WriteLine("This method will double a number and return it.\n");
+            Console.WriteLine("Please enter a number on the nect line.\n");
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            sum *= 2;
+            Console.WriteLine(sum);
+            return sum; 
+        }
+
+        
+
+
+
+  
