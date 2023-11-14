75ea3ca99e22d9f1f057ebd0ce582cdeebf0deaf
NoahET <s12097572@students.duvalschools.org>
2023-11-14 14:15:52 -0500

    v.1 grey escape
    partial

---
+++ 05a_exampleGameMethods/ProgramTemplate.cs
@@ -0,0 +1,42 @@
+// Noah Thompson, Rubric, v0.1
+using system;
+
+namespace Grey Escape
+{   
+    class program
+    {
+       static void Main(string[] args); 
+
+       static void MethodOne()
+       {
+        
+       }
+
+        static void Methodtwo()
+       {
+        
+       }
+
+        static void MethodThree()
+       {
+        
+       }
+
+        static void MethodFour()
+       {
+        
+       }
+
+        static int DiceRoll()
+       { // random number gen 1 to 6
+         int DiceRoll = rnd.Next(1,6); //  1-3 = low vaue  4-5 = high value 
+
+        return DiceRoll
+        
+        Console.WriteLine(" You rolled a  dice number,you move number on dice after roll")
+       }
+        
+    
+    
+    }
+}
