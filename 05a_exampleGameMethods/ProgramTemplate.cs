e86f55941ec8feecd436c03e4bc573790fcb6cbc
Ryan Kelley <kelleyr@duvalschools.org>
2023-12-07 14:12:59 -0500

    Code Review Test -- 12-07-23

--- 05a_exampleGameMethods/ProgramTemplate.cs
+++ 05a_exampleGameMethods/ProgramTemplate.cs
@@ -22,15 +22,17 @@ namespace GreyEscape
        }
 
         static void WrongRight()
-       { int =choices = 1,2,3,
-        // if wrong chouice then print "Wrong, try again"/ if right Print"your rigth , the next step to escaping was made"
-        if (choices == 1) { console.writeline("your wrong, you lose a guess be more carefull. try again");
-        }
-        if (choices == 2) { console.writeline("your rigth , the next step to escaping was made");
-        }
-        if (choices == 3) { console.writeline(" Meh, its not totally wrong or rigth. move on ");
-        }
-        if else console.WriteLine("just choose one of them or stop playing");
+       { 
+            int choices = 0; 
+            // if wrong chouice then print "Wrong, try again"/ if right Print"your rigth , the next step to escaping was made"
+            // After the first if all others should be else if and the final one should be else.  
+            if (choices == 1) { console.writeline("your wrong, you lose a guess be more carefull. try again");
+            }
+            if (choices == 2) { Console.WriteLine("your rigth , the next step to escaping was made");
+            }
+            if (choices == 3) { Console.WriteLine(" Meh, its not totally wrong or rigth. move on ");
+            }
+            if else Console.WriteLine("just choose one of them or stop playing");
 
         static void GuessCounter()
        {
