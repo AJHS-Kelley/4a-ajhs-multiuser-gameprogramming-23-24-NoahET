a7750a3e0caadb4c0c4ba24efc66d2dd87b2e852
NoahET <77453987+NoahET@users.noreply.github.com>
2024-01-10 14:07:45 -0500

    review v.01
     char bool, floast,int ect

---
+++ 05a_exampleGameMethods/06_review.cs/06_review.cs
@@ -0,0 +1,56 @@
+v.1
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static void Main(string[] argas)
+        { 
+            //declaring and assigning variables
+            // integer -- -2.147,483,648 to 2,147,483,647
+            // interger = 4 bytes
+            int nuber = 9;
+
+            //long -- -9,232,372,062,432,543,349,5
+            // long = 8 bytes
+            long bignum = 087947652453725925;
+
+            // float
+            //
+            float thud = 5.37f;
+
+            // double -- stores up to 15 decimal digits
+            // double holds 8 bytes
+            double doublethud = 7.485782674782;
+            
+            // boolean --true/false
+            // bollean = 1 bit
+            bool crash = false;
+            
+            // string
+            // 2-bytes per charcter 
+            string words = "nubins";
+
+            //character (char)
+            //single character, in single qoutes
+            //2-bytes
+            char bowd = 'F';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
